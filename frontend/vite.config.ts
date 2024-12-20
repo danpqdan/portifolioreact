831f4788758c605647adac085f43cfd2510ad819
Daniel <danieltisantos@gmail.com>
2024-12-19 21:23:06 -0300

    FIX: interpolation

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import {apiUrl} from './src/utils/ExportRoute'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 80,
-    host: true
+    host: true,
+    proxy: {
+      '/api': {
+        target: apiUrl,
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+
   },
   base: "/",
 })
