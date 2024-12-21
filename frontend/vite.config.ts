288e3086147eaa4166c7d078e48d620b9dfe0744
Daniel <danieltisantos@gmail.com>
2024-12-21 13:34:16 -0300

    REMOVE: unsert variable

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,5 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { apiUrl } from './src/utils/ExportRoute'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
