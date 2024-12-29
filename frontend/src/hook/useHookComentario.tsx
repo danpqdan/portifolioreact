ec728f9a771e0ed424aab3ff5e331bdfb25cdb51
Daniel <danieltisantos@gmail.com>
2024-12-29 17:40:18 -0300

    FINALLY: imp api at avaliable consult

--- frontend/src/hook/useHookComentario.tsx
+++ frontend/src/hook/useHookComentario.tsx
@@ -13,7 +13,7 @@ export const useGetComentarios = () => {
     useEffect(() => {
         const fetchComentarios = async () => {
             try {
-                const res = await fetch("https://api.dsplayground/api/review", {
+                const res = await fetch("https://api.dsplayground.com.br/api/review", {
 
                     method: "GET",
                     credentials: 'include'
@@ -38,7 +38,7 @@ export const useGetComentarios = () => {
 export const usePostComentario = () => {
     const postComentario = async (comentario: Comentario): Promise<Response> => {
         try {
-            const response = await fetch("https://api.dsplayground/api/review", {
+            const response = await fetch("https://api.dsplayground.com.br/api/review", {
                 credentials: 'include',
                 method: 'POST',
                 headers: {
