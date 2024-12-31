fbaae425ae697a9d229de62f39a8a4875790a3e8
ds-ubunto@desktop <danieltisantos@gmail.com>
2024-12-31 18:36:12 -0300

    ADD: implement user registration form with validation and styling

--- frontend-services/src/App.tsx
+++ frontend-services/src/App.tsx
@@ -1,14 +1,41 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import { Registro } from './components/registro';
 
-function App() {
-  const [count, setCount] = useState(0)
+export function App() {
+
+  const [isShow, setIsShow] = useState(false);
+
+  const toogleBar = () => {
+    setIsShow(prevState => !prevState);
+  }
 
   return (
-    <>
-      
-    </>
-  )
+    <div className="App">
+      <div className='header'>
+        <h1>My App</h1>
+        <button id='barInit' onClick={toogleBar}> X </button>
+        {isShow && (
+          <div id='bar'>
+            <a className='itensNavigate' href='#'><button>Home</button></a>
+            <a className='itensNavigate' href='#'><button>About</button></a>
+            <a className='itensNavigate' href='#'><button>Contact</button></a>
+          </div>
+        )}
+      </div >
+      <div className='content-container'>
+        <Registro />
+        <div>
+          <div className='buttons-options'>
+            <button>Torne-se vendedor</button>
+            <button>Teste anonimo</button>
+          </div>
+
+        </div>
+      </div>
+
+
+    </div>
+  );
 }
 
-export default App
---
+++ frontend-services/src/components/registro.tsx
@@ -0,0 +1,99 @@
+import axios from 'axios';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import '../css/registro.css';
+
+interface User {
+    username: string;
+    email: string;
+    senha: string;
+    confirmarSenha: string;
+    privacyPolicy: boolean;
+    pushNotification: boolean;
+}
+
+export const Registro: React.FC = () => {
+    const [user, setUser] = useState<User>({
+        username: '',
+        email: '',
+        senha: '',
+        confirmarSenha: '',
+        privacyPolicy: false,
+        pushNotification: false,
+    });
+
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+
+        if (user.senha !== user.confirmarSenha) {
+            alert('Senhas não conferem');
+            return;
+        }
+
+        try {
+            await axios.post('http://localhost:8080/auth/singup/usuario', user);
+
+            alert('Usuário cadastrado com sucesso');
+            navigate('/login');
+        } catch (error: any) {
+            console.error('Error during user registration:', error);
+            alert('Erro ao cadastrar usuário');
+        }
+    };
+    return (
+        <div className="container">
+            <h1>Registre-se</h1>
+            <form onSubmit={handleSubmit}>
+                <input
+                    placeholder="Usuário"
+                    value={user.username}
+                    onChange={(e) => setUser({ ...user, username: e.target.value })}
+                />
+                <input
+                    type="email"
+                    placeholder="Email"
+                    value={user.email}
+                    onChange={(e) => setUser({ ...user, email: e.target.value })}
+                />
+                <input
+                    type="password"
+                    placeholder="Senha"
+                    value={user.senha}
+                    onChange={(e) => setUser({ ...user, senha: e.target.value })}
+                />
+                <input
+                    type="password"
+                    placeholder="Confirmar Senha"
+                    value={user.confirmarSenha}
+                    onChange={(e) => setUser({ ...user, confirmarSenha: e.target.value })}
+                />
+                <br />
+
+                <div className='label-content'>
+                    <input
+                        className='input-checkbox'
+                        type="checkbox"
+                        checked={user.privacyPolicy}
+                        onChange={(e) => setUser({ ...user, privacyPolicy: e.target.checked })}
+                    />
+                    <p className='checkbox-paragrafo'>Aceito a política de privacidade</p>
+
+                    <input
+                        className='input-checkbox'
+                        type="checkbox"
+                        checked={user.pushNotification}
+                        onChange={(e) => setUser({ ...user, pushNotification: e.target.checked })}
+                    />
+                    <p className='checkbox-paragrafo'>Autorizar notificações push (opcional)</p>
+                </div>
+                <div className='button-content'>
+                    <button type="submit">Enviar</button>
+                    <button>Limpar</button>
+                </div>
+            </form>
+        </div>
+    );
+};
+
