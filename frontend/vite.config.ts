b2b49050050aed7e45f646cfb464f766f38ed677
ds-ubunto@desktop <danieltisantos@gmail.com>
2025-01-30 21:30:15 -0300

    [UPDATE] bump version to 0.1.2, add new dependencies, and configure HTTPS for Vite server

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,16 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import fs from 'fs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 80,
-    https: true
+    https: {
+      key: fs.readFileSync('key.pem'),
+      cert: fs.readFileSync('cert.pem'),
+    }
   }
-})
+});
+
